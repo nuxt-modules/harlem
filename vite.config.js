b4d57df9f5b44f83f2c47e9ab9ff362bf22b707b
Daniel Roe <daniel@roe.dev>
2022-02-23 11:07:37 +0000

    ci: add build command

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   test: {
     coverage: {
-      include: ['dist'],
+      include: ['src', 'dist'],
       reporter: ['text', 'json'],
     },
   },
