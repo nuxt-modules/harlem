74ee5b38af3fc1491edb77feb1eabc2baf5fbda9
Daniel Roe <daniel@roe.dev>
2022-02-23 11:46:41 +0000

    test: add basic e2e test suite (#8)

---
+++ test/e2e/index.spec.ts
@@ -0,0 +1,18 @@
+import { fileURLToPath } from 'url'
+import { $fetch, setup } from '@nuxt/test-utils'
+import { describe, expect, it } from 'vitest'
+
+const playground = fileURLToPath(new URL('../../playground', import.meta.url))
+
+await setup({
+  rootDir: playground,
+  server: true,
+  browser: false,
+})
+
+describe('module', () => {
+  it('should render the home page', async () => {
+    const html = await $fetch('/')
+    expect(html).toContain('<h1>Hello John Doe</h1>')
+  }, 50000)
+})
