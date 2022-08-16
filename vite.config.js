dd5638949dedb9d020534a1be6a99b586585fde3
Daniel Roe <daniel@roe.dev>
2022-08-16 12:41:26 +0200

    chore: inline test utils edge

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   test: {
+    deps: {
+      inline: ['@nuxt/test-utils-edge'],
+    },
     coverage: {
       include: ['src', 'dist'],
       reporter: ['text', 'json'],
