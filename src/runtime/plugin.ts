e17d43a6686ec175f7e1f07704b0e2de8bea994e
Lexpeartha <43365376+Lexpeartha@users.noreply.github.com>
2023-03-28 15:58:19 +0200

    feat: upgrade to harlem v3 (#226)

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,34 +1,22 @@
-import Harlem from '@harlem/core'
-import {
-  createClientSSRPlugin,
-  createServerSSRPlugin,
-  // getBridgingScript,
-  getBridgingScriptBlock,
-} from '@harlem/plugin-ssr'
+import { createVuePlugin } from '@harlem/core'
+import { createClientSSRPlugin, createServerSSRPlugin, getBridgingScript } from '@harlem/plugin-ssr'
 
 import { defineNuxtPlugin } from '#app'
+import { useHead } from '#imports'
 import harlemPlugins from '#build/harlem-plugins'
 
 export default defineNuxtPlugin(nuxtApp => {
-  nuxtApp.vueApp.use(Harlem, {
-    plugins: [
-      ...harlemPlugins.map(p => p()),
-      process.client && createClientSSRPlugin(),
-      process.server && createServerSSRPlugin(),
-    ].filter(Boolean),
+  nuxtApp.vueApp.use(
+    createVuePlugin({
+      plugins: [
+        ...harlemPlugins.map(p => p()),
+        process.client && createClientSSRPlugin(),
+        process.server && createServerSSRPlugin(),
+      ].filter(Boolean),
+    })
+  )
+
+  useHead({
+    script: [{ children: () => getBridgingScript() }],
   })
-
-  // Workaround until we have support in vueuse/head
-  if (process.server && nuxtApp.ssrContext && 'renderMeta' in nuxtApp.ssrContext) {
-    const originalRender = nuxtApp.ssrContext.renderMeta
-    nuxtApp.ssrContext.renderMeta = async () => {
-      const result = originalRender ? await originalRender() : {}
-      result.bodyScripts = (result.bodyScripts || '') + getBridgingScriptBlock()
-      return result
-    }
-  }
-
-  // useHead({
-  //   script: [{ children: computed(() => getBridgingScript()) }],
-  // })
 })
