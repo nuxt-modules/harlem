9b7803d913780bfa7ed7b4402f4162aaad31730c
Daniel Roe <daniel@roe.dev>
2023-03-28 14:33:36 +0100

    style: lint

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -6,13 +6,15 @@ import { useHead } from '#imports'
 import harlemPlugins from '#build/harlem-plugins'
 
 export default defineNuxtPlugin(nuxtApp => {
-  nuxtApp.vueApp.use(createVuePlugin({
-    plugins: [
-      ...harlemPlugins.map(p => p()),
-      process.client && createClientSSRPlugin(),
-      process.server && createServerSSRPlugin(),
-    ].filter(Boolean),
-  }))
+  nuxtApp.vueApp.use(
+    createVuePlugin({
+      plugins: [
+        ...harlemPlugins.map(p => p()),
+        process.client && createClientSSRPlugin(),
+        process.server && createServerSSRPlugin(),
+      ].filter(Boolean),
+    })
+  )
 
   useHead({
     script: [{ children: () => getBridgingScript() }],
