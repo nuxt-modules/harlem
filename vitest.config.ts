27e7a136b91d04e14c6fc4367d7f3636b9f55ac8
Daniel Roe <daniel@roe.dev>
2023-12-04 08:49:56 +0000

    chore: rename vite -> vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    deps: {
-      inline: ['@nuxt/test-utils-edge'],
-    },
     coverage: {
       include: ['src', 'dist'],
       reporter: ['text', 'json'],
